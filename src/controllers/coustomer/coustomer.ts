7500074513027b34f42b76a801e021e173eb335d
navin kumar <navinkumar02929@gmail.com>
2024-08-20 18:43:46 +0530

    api change in customer

--- src/controllers/coustomer/coustomer.ts
+++ src/controllers/coustomer/coustomer.ts
@@ -204,7 +204,7 @@ public deleteCoustomer = async (req:Request, res:Response)=>{
         condition._id = req.params.id;
       }
 
-      const customers = await Coustomer.find(condition).select('name _id').populate([
+      const customers = await Coustomer.find(condition).populate([
         {path: 'UserID',select:'name'},
         {path: 'ProductID',select: 'name'},
 
